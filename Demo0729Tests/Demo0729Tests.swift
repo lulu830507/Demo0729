b21ba7f4e0fe676c9f44c9379c6a9532f170c9ac
ＬＵＬＵ <lulu830507@gmail.com>
2021-07-29 15:28:02 +0800

    Initial Commit

---
+++ Demo0729Tests/Demo0729Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Demo0729Tests.swift
+//  Demo0729Tests
+//
+//  Created by 林思甯 on 2021/7/29.
+//
+
+import XCTest
+@testable import Demo0729
+
+class Demo0729Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
